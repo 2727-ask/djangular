3f413753c4080f4d3a579ddc9e1eae4cdaf811e1
ashutosh-indianpix <99986604+ashutosh-indianpix@users.noreply.github.com>
2022-04-19 21:17:07 +0530

    whitenoise added

--- techvercetti/settings/base.py
+++ techvercetti/settings/base.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -129,5 +130,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
