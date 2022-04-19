90f755caa4dbff1453ee9cf5c8e8a451371adc1d
ashutosh-indianpix <99986604+ashutosh-indianpix@users.noreply.github.com>
2022-04-19 19:12:42 +0530

    psycopg2

---
+++ techvercetti/settings/prod.py
@@ -0,0 +1,11 @@
+from .base import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'djangularprod',
+        'USER': 'ashutoshkumbhar',
+        'PASSWORD': 'Asdfghjkl@27',
+        'PORT': '5432',
+    }
+}
