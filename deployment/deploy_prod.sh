52bf0ee74126aefe167c20fe2963cfcc31617ef5
ashutosh-indianpix <99986604+ashutosh-indianpix@users.noreply.github.com>
2022-04-20 23:12:43 +0530

    JenKins Fix

--- deployment/deploy_prod.sh
+++ deployment/deploy_prod.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 ssh root@64.227.176.176 <<EOF
-  cd projectdir/djangular
-  git pull
+  cd projectdir
   source env/bin/activate
+  cd djangular
+  git pull
   pip install -r requirements.txt
   ./manage.py migrate
   sudo supervisorctl restart djangular
