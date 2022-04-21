f3614565b60a5b79464827caa2cb345699701ecb
ashutosh-indianpix <99986604+ashutosh-indianpix@users.noreply.github.com>
2022-04-21 15:09:13 +0530

    sbhbd

--- App/views.py
+++ App/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Category
 from .serializers import CategorySerializer
+
 # Create your views here.
 
 class CategoryAPIView(APIView):
