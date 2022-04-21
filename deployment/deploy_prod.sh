db3f70e5f031a9205b48588020802d4430c57749
ashutosh-indianpix <99986604+ashutosh-indianpix@users.noreply.github.com>
2022-04-21 15:27:55 +0530

    Deploy Shell Script Added

--- deployment/deploy_prod.sh
+++ deployment/deploy_prod.sh
@@ -4,6 +4,8 @@ ssh root@64.227.176.176 <<EOF
   cd projectdir
   source env/bin/activate
   cd djangular
+  git add .
+  git commit -m "Server Commit Made"
   git pull
   pip install -r requirements.txt
   ./manage.py migrate
