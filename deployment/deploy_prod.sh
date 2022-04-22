dd0f596087e8c76da4f1f269c017e2c0b2160e0c
ashutosh-indianpix <99986604+ashutosh-indianpix@users.noreply.github.com>
2022-04-22 09:50:09 +0530

    Api Reconditioned

--- deployment/deploy_prod.sh
+++ deployment/deploy_prod.sh
@@ -6,7 +6,7 @@ ssh root@64.227.176.176 <<EOF
   cd djangular
   git add .
   git commit -m "Server Commit Made"
-  git pull
+  git pull origin main
   pip install -r requirements.txt
   ./manage.py migrate
   sudo supervisorctl restart djangular
