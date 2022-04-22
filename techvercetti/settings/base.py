97acc3b85a1fcaf5332dd0f455391ee6f85ebb2e
ashutosh-indianpix <99986604+ashutosh-indianpix@users.noreply.github.com>
2022-04-22 10:52:34 +0530

    Static Files Root Changed

--- techvercetti/settings/base.py
+++ techvercetti/settings/base.py
@@ -128,4 +128,4 @@ import os
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
-STATIC_ROOT  = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT  = os.path.join(BASE_DIR, 'static')
