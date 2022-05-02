1e991178b3fcaf8e21831dc2d615cd14b1fc0a26
ashutosh-indianpix <99986604+ashutosh-indianpix@users.noreply.github.com>
2022-05-02 18:48:03 +0530

    Removed Parent

--- App/models.py
+++ App/models.py
@@ -5,6 +5,5 @@ class Category(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=61)
     description = models.TextField(null=True)
-    parent = models.ForeignKey('self',null=True,on_delete=models.CASCADE,blank=True)
     def __str__(self):
         return self.title
