f92d442477ca4d848968afb0b373f0b4d639fb6c
ashutosh-indianpix <99986604+ashutosh-indianpix@users.noreply.github.com>
2022-05-02 15:46:53 +0530

    JWT Token Added

--- App/urls.py
+++ App/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
+from rest_framework_jwt.views import obtain_jwt_token,refresh_jwt_token,verify_jwt_token
 from . import views
 urlpatterns = [
     path('welcome',views.WelcomeAPI.as_view(),name="WelcomeApiView"),
     path('category',views.CategoryAPIView.as_view(),name="CategoryAPIView"),
-    path('category/<int:pk>',views.CategoryAPIView.as_view())
+    path('category/<int:pk>',views.CategoryAPIView.as_view()),
+    path('auth-jwt',obtain_jwt_token),
+    path('refresh-jwt', refresh_jwt_token),
+    path('verify-jwt',verify_jwt_token)
 ]
