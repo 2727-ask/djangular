04edce7f665d28bb386d569c12537af74b4bf5d2
Ashutosh Kumbhar <ashutoshkumbhar@Ashutoshs-MacBook-Air.local>
2022-07-24 16:26:27 +0530

    re

--- App/urls.py
+++ App/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from rest_framework_jwt.views import obtain_jwt_token,refresh_jwt_token,verify_jwt_token
 from .Views import categoryAPIView, blogpostAPIView, signUpAPIView,profileAPIView
 urlpatterns = [
-    path('category',categoryAPIView.CategoryAPIView.as_view(),name="categoryAPIView.py"),
-    path('category/<int:pk>',categoryAPIView.CategoryAPIView.as_view()),
+    path('api/category',categoryAPIView.CategoryAPIView.as_view(),name="categoryAPIView.py"),
+    path('api/category/<int:pk>',categoryAPIView.CategoryAPIView.as_view()),
     path('api/blog_post/',blogpostAPIView.BlogPostAPIView.as_view()),
     path('api/signup/',signUpAPIView.SignUpAPIView.as_view()),
     path('api/profile/',profileAPIView.ProfileAPIView.as_view()),
